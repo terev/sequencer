a8c12497fdefdb81d010d9f165fba4f6244e2b7c
TrevorFoster <trevor.foster@hotmail.ca>
2017-02-13 01:43:48 -0500

    -Adding simple server for editing and rendering the diagrams -page with editor and diagram view started

--- index.js
+++ index.js
@@ -2,94 +2,101 @@ const peg = require('pegjs');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const _ = require('lodash');
-const test_files = [{name: 'test.seq'}];
 const Canvas = require('canvas');
 let parser;
-var crypto = require('crypto');
+const crypto = require('crypto');
 
-function toPng(seq) {
-	const canvas = new Canvas(800, 600);
-	const ctx = canvas.getContext('2d');
-	const out = fs.createWriteStream(__dirname + '/diagram.png');
-	const stream = canvas.pngStream();
-	ctx.font = '30px Impact';
+module.exports.draw = function (seq, width, height) {
+    const canvas = new Canvas(width, height);
+    const ctx = canvas.getContext('2d');
+    ctx.font = '20px Impact';
+    ctx.antialias = 'gray';
 
-	stream.on('data', function(chunk){
-		out.write(chunk);
-	});
-	stream.on('end', function(){
-		console.log('saved png');
-	});
+    let participants = {};
+    let x = 10;
+    let y = 10;
+    _.each(seq, thing => {
+        if (thing.type == 'interaction') {
+            if (!_.has(participants, thing.left)) {
+                let size = ctx.measureText(thing.left);
+                let lifeline = x + size.width / 2;
 
-	let participants = {};
-	let x = 10;
-	_.each(seq, thing => {
-		if(thing.type == 'interaction') {
-			if(!_.has(participants, thing.left)) {
-				let size = ctx.measureText(thing.left);
-				let lifeline = x + size.width / 2;
+                console.log(size);
 
-				ctx.fillStyle = '#09F';
-				ctx.fillRect(x, 100 - size.emHeightAscent, size.width, size.emHeightAscent);
-				participants[thing.left] = {x: x};
-				ctx.fillStyle = '#FFFFFF';
-				ctx.fillText(thing.left, x, 100);
-				ctx.beginPath();
-				ctx.moveTo(lifeline, 100);
-				ctx.lineTo(lifeline, 800);
-				ctx.stroke();
-				ctx.closePath();
-				x += size.width + 10;
-			}
-			if(!_.has(participants, thing.right)) {
-				let size = ctx.measureText(thing.right);
-				let lifeline = x + size.width / 2;
+                ctx.fillStyle = '#09F';
+                ctx.fillRect(x, y, size.width, size.emHeightAscent);
+                participants[thing.left] = {x: x};
+                ctx.fillStyle = '#FFFFFF';
+                ctx.fillText(thing.left, x, y + size.emHeightAscent);
+                ctx.beginPath();
+                ctx.moveTo(lifeline, y + size.emHeightAscent);
+                ctx.lineTo(lifeline, height);
+                ctx.stroke();
+                ctx.closePath();
+                x += size.width + 10;
+            }
+            if (!_.has(participants, thing.right)) {
+                let size = ctx.measureText(thing.right);
+                let lifeline = x + size.width / 2;
 
-				ctx.fillStyle = '#09F';
-				ctx.fillRect(x, 100 - size.emHeightAscent, size.width, size.emHeightAscent);
-				participants[thing.right] = {x: x};
-				ctx.fillStyle = '#FFFFFF';
-				ctx.fillText(thing.right, x, 100);
-				ctx.beginPath();
-				ctx.moveTo(lifeline, 100);
-				ctx.lineTo(lifeline, 800);
-				ctx.stroke();
-				ctx.closePath();
-				x += size.width + 10;
-			}
-		}
-	});
+                ctx.fillStyle = '#09F';
+                ctx.fillRect(x, y, size.width, size.emHeightAscent);
+                participants[thing.right] = {x: x};
+                ctx.fillStyle = '#FFFFFF';
+                ctx.fillText(thing.right, x, y + size.emHeightAscent);
+                ctx.beginPath();
+                ctx.moveTo(lifeline, y + size.emHeightAscent);
+                ctx.lineTo(lifeline, height);
+                ctx.stroke();
+                ctx.closePath();
+                x += size.width + 10;
+            }
+        }
+    });
+
+    return canvas;
+};
+
+module.exports.parse = function (sequence) {
+    return Promise.try(() => {
+        return parser.parse(sequence);
+    });
+};
+
+function fromCache() {
+    let parser = require(__dirname + '/source');
+    console.log('loaded from cache');
+    return parser;
 }
 
-function getParser(contents) {
-	return peg.generate(contents.toString(), {optimize: 'speed'});
+function loadParser(hash, contents) {
+    fs.writeFileSync(__dirname + 'latest', hash);
+    fs.writeFileSync(__dirname + 'source.js', peg.generate(contents.toString(), {
+        optimize: 'speed',
+        output: 'source',
+        format: 'commonjs'
+    }));
+    parser = peg.generate(contents.toString(), {optimize: 'speed'});
+    console.log('regenerated parser');
 }
 
-fs.readFileAsync('latest').then((latest) => {
-	return fs.readFileAsync('sequence.pegjs').then(contents => {
-		var md5sum = crypto.createHash('md5');
-		let hash = md5sum.update(contents.toString()).digest('hex');
-		
-		if(latest.toString() !== hash) {
-			fs.writeFileSync('latest', hash);
-			fs.writeFileSync('source.js', peg.generate(contents.toString(), {optimize: 'speed', output: 'source', format: 'commonjs'}));
-			parser = peg.generate(contents.toString(), {optimize: 'speed'});
-			console.log('regenerated parser');
-		} else {
-			parser = require('./source');
-			console.log('from cache');
-		}
+fs.readFileAsync(__dirname + '/sequence.pegjs').then(contents => {
+    const md5sum = crypto.createHash('md5');
+    let hash = md5sum.update(contents.toString()).digest('hex');
 
-		return parser;
-	})
-})
-.then(parser => {
-	return Promise.map(test_files, file => {
-		return fs.readFileAsync(file.name)
-			.then(contents => {
-				let parsed = parser.parse(contents.toString());
-				console.log(parsed);
-				toPng(parsed);
-			});
-	});
+    return fs.accessAsync(__dirname + '/latest', fs.constants.R_OK | fs.constants.W_OK)
+        .then(() => {
+            return fs.readFileAsync(__dirname + '/latest').then((latest) => {
+                if (latest.toString() === hash) {
+                    parser = fromCache();
+                } else {
+                    parser = loadParser(hash, contents);
+                }
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            parser = loadParser(hash, contents);
+            return Promise.resolve(parser);
+        });
 });
---
+++ server/server.js
@@ -0,0 +1,45 @@
+// server.js
+// load the things we need
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const fs = require('fs');
+
+const sequencer = require('../index');
+
+app.use(express.static(__dirname + '/../ace'));
+app.use('/axios', express.static(__dirname + '/../node_modules/axios'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function (req, res) {
+    let initial = fs.readFileSync('../test.seq');
+
+    res.render(__dirname + '/../views/index.ejs',
+        {
+            sequence: initial.toString()
+        });
+});
+
+app.post('/diagram', function(req, res) {
+    sequencer.parse(req.body.interaction)
+        .then((seq) => {
+            console.log(seq);
+            res.setHeader('Content-Type', 'image/png');
+            res.send(sequencer.draw(seq, req.body.width, req.body.height).toDataURL());
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).send('failed');
+        });
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
